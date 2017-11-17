1a3b0b59b28f5906e403ca165611511f489c3e61
DManstrator <DManstrator@gmail.com>
2017-11-17 15:54:44 +0100

    Implemented Structure and Contents of Example Project

---
+++ src/main/java/ly/muasica/base/activity/ActivityRepository.java
@@ -0,0 +1,7 @@
+package ly.muasica.base.activity;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActivityRepository extends CrudRepository<Activity, Long>  {
+	
+}
--- src/main/java/ly/muasica/base/hello/Greeting.java
+++ src/main/java/ly/muasica/base/hello/Greeting.java
@@ -1,4 +1,4 @@
-package hello;
+package ly.muasica.base.hello;
 
 public class Greeting {
 
