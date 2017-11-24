86a54fc57b9a6cfd47fbda7a025e32931e56a796
Gabl <hm-gabl@hm.edu>
2017-11-24 15:10:08 +0100

    Change Activity Class to use Lists for Tags and Attachments. Added a Test for Activity

--- src/main/java/ly/muasica/base/activity/Activity.java
+++ src/main/java/ly/muasica/base/activity/Activity.java
@@ -1,9 +1,14 @@
 package ly.muasica.base.activity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import ly.muasica.base.activity.structure.Attachment;
+import ly.muasica.base.activity.structure.Tag;
 
 @Entity
 public class Activity {
@@ -12,15 +17,17 @@ public class Activity {
     @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
     private String text;
-    private String tags;
+//    private List<Tag> tags;  // TODO Write Tag Class
+//    private List<Attachment> attachments;  // TODO Write Attachment Class
     private String title;
 
     public Activity (){};
 
-    public Activity(String text, String tags, String title) {
+    public Activity(String text, String title) {
         this.text = text;
-        this.tags = tags;
         this.title = title;
+//        this.tags = tags;
+//        this.attachments = attachments;
     }
 
     public Long getId() {
@@ -38,14 +45,6 @@ public class Activity {
     public void setText(String text) {
         this.text = text;
     }
-    
-    public String getTags() {
-      return tags;
-    }
-
-    public void setTags(String tags) {
-      this.tags = tags;
-    }
 
     public String getTitle() {
         return title;
@@ -54,4 +53,20 @@ public class Activity {
     public void setTitle(String title) {
         this.title = title;
     }
+
+//	public List<Tag> getTags() {
+//		return tags;
+//	}
+//
+//	public void setTags(List<Tag> tags) {
+//		this.tags = tags;
+//	}
+//
+//	public List<Attachment> getAttachments() {
+//		return attachments;
+//	}
+//
+//	public void setAttachments(List<Attachment> attachments) {
+//		this.attachments = attachments;
+//	}
 }
