fc08ae70fcfd5daa084ec272100f064a0de12f92
DManstrator <DManstrator@gmail.com>
2017-12-14 18:44:18 +0100

    Changed HomeController to check for Cookie

--- src/main/java/ly/muasica/base/HomeController.java
+++ src/main/java/ly/muasica/base/HomeController.java
@@ -1,14 +1,42 @@
 package ly.muasica.base;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import ly.muasica.base.auth.AuthRepository;
+import ly.muasica.base.auth.User;
 
 @Controller // so framework can recognize this as a controller class
 @RequestMapping("/")
 public class HomeController {
-
-    @GetMapping
-    public String index() { return "activities.html"; }
+	
+    @Autowired
+    private AuthRepository authRepository;
+    
+	@RequestMapping()
+    public String index(HttpServletRequest req) {
+		Cookie[] cookies = req.getCookies();
+		if (cookies != null)  {
+			for (Cookie cookie : cookies)  {
+				if (cookie.getName().equals("MUASiCaly"))  {
+					Iterable<User> users = authRepository.findAll();
+					for (User user : users)  {
+						System.out.println(user.getCookie().getValue());  // TODO fix NPE
+						System.out.println(cookie.getValue());
+						if (user.getCookie().getValue().equals(cookie.getValue()))  {
+							System.out.println("Cookie fits");
+							return "activities.html";
+						}
+					}
+				}
+			}
+		}
+		
+		return "index.html";
+    }
 
 }
